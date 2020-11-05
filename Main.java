3ddc3a7088c28e45b7d409f0f578bb3e6b06ccbc
GabrialStanko15 <gabrial.stanko@patelhs.org>
2020-11-05 17:44:20 +0000

    added scanner

--- Main.java
+++ Main.java
@@ -1,12 +1,9 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
 
     int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-
-    string[] names = { "Jack", "John", "Mark", "Chris", "Casey", "Breanna", "Joan", "Jessie", "Micheal", "Sarah", "Donald", "Mike", "Kamala", "Mitch", "Rob" };
-
-    for (int i = 14; i >= 0; i++) {
-      System.out.println(names[i]);
-    }
+  System.out.println("Enter your name");
+  String name = scan.next();
   }
-}
+} 
