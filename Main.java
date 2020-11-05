dd141f298983d3577e342a65d849366c55dd026d
GabrialStanko15 <gabrial.stanko@patelhs.org>
2020-11-05 17:20:59 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+
+    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+    string[] names = { "Jack", "John", "Mark", "Chris", "Casey", "Breanna", "Joan", "Jessie", "Micheal", "Sarah", "Donald", "Mike", "Kamala", "Mitch", "Rob" };
+
+    for (int i = 14; i >= 0; i++) {
+      System.out.println(names[i]);
+    }
+  }
+}
