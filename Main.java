0a7a5e337614615fd365c7de210ab08af5b48341
GabrialStanko15 <gabrial.stanko@patelhs.org>
2020-11-09 15:36:28 +0000

    Added scanners

--- Main.java
+++ Main.java
@@ -1,9 +1,57 @@
 import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-
+    Scanner scan = new Scanner(System.in);
     int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-  System.out.println("Enter your name");
-  String name = scan.next();
+    System.out.println("Enter your name");
+    String name = scan.next();
+   System.out.println(name + " Your seat is " + seats[0]);
+   System.out.println("Enter your name");
+    String nameTwo = scan.next();
+    System.out.println(nameTwo + " Your seat is " + seats[1]);
+    System.out.println("Enter your name");
+    String nameThree = scan.next();
+    System.out.println(nameThree + " Your seat is " + seats[2]);
+    System.out.println("Enter your name");
+    String nameFour = scan.next();
+    System.out.println(nameFour + " Your seat is " + seats[3]);
+    System.out.println("Enter your name");
+    String nameFive = scan.next();
+    System.out.println(nameFive + " Your seat is " + seats[4]);
+    System.out.println("Enter your name");
+    String nameSix = scan.next();
+    System.out.println(nameSix + " Your seat is " + seats[5]);
+    System.out.println("Enter your name");
+    String nameSeven = scan.next();
+    System.out.println(nameSeven + " Your seat is " + seats[6]);
+    System.out.println("Enter your name");
+    String nameEight = scan.next();
+    System.out.println(nameEight + " Your seat is " + seats[7]
+    );
+    System.out.println("Enter your name");
+    String nameNine = scan.next();
+    System.out.println(nameNine + " Your seat is " + seats[8]);
+    System.out.println("Enter your name");
+    String nameTen = scan.next();
+    System.out.println(nameTen + " Your seat is " + seats[9]);
+    System.out.println("Enter your name");
+    String nameEleven = scan.next();
+    System.out.println(nameEleven + " Your seat is " + seats[10]);
+    System.out.println("Enter your name");
+    String nameTwelve = scan.next();
+    System.out.println(nameTwelve + " Your seat is " + seats[11]);
+    System.out.println("Enter your name");
+    String nameThirteen = scan.next();
+    System.out.println(nameThirteen + " Your seat is " + seats[12]);
+    System.out.println("Enter your name");
+    String nameFourteen = scan.next();
+    System.out.println(nameFourteen + " Your seat is " + seats[13]);
+    System.out.println("Enter your name");
+    String nameFiveteen = scan.next();
+    System.out.println(nameFiveteen + " Your seat is " + seats[14]);
+
+    for
+
   }
-} 
+}
